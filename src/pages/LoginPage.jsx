6c9f448138802ca0c3957bfd51327688f114cb5f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-22 09:28:55 +0300

    styled form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,10 +6,32 @@ const Title = styled.h1`
   font-weight: 500;
 `;
 
+const Input = styled.input`
+  font-size: 16px;
+  padding: 0.3em 0.8em;
+  border-radius: 4px;
+  border: 1px solid #777;
+  display: block;
+  width: 300px;
+  margin-bottom: 10px;
+`;
+
+const SubmitBnt = styled.button.attrs({
+  type: 'submit',
+})`
+  background-color: #333;
+  color: white;
+`;
+
 export default function LoginPage() {
   return (
     <Wrap>
       <Title>Login here</Title>
+      <form>
+        <Input type='text' placeholder='Email' id='email' />
+        <Input type='password' placeholder='Password' id='password' />
+        <SubmitBnt>Login</SubmitBnt>
+      </form>
     </Wrap>
   );
 }
--- src/pages/VipPage.jsx
+++ src/pages/VipPage.jsx
@@ -13,15 +13,15 @@ export default function VipPage() {
       <p>
         Welcome to our exclusive VIP page, where luxury and exceptional
         experiences await you at every click. As a valued member of our inner
-        circle, you're about to embark on a journey beyond the ordinary. Here,
-        indulgence knows no bounds, and your desires take center stage. From
-        curated content crafted specifically for our VIPs to unparalleled access
-        to premium services, this is your gateway to a world where
+        circle, you&apos;re about to embark on a journey beyond the ordinary.
+        Here, indulgence knows no bounds, and your desires take center stage.
+        From curated content crafted specifically for our VIPs to unparalleled
+        access to premium services, this is your gateway to a world where
         sophistication meets innovation. Join us as we redefine exclusivity and
         grant you a key to unlock a realm of privileges designed to cater to
-        your every whim. Your VIP status is not just a label; it's an invitation
-        to explore a realm of opulence reserved solely for those who seek the
-        finest things in life.
+        your every whim. Your VIP status is not just a label; it&apos;s an
+        invitation to explore a realm of opulence reserved solely for those who
+        seek the finest things in life.
       </p>
     </Wrap>
   );
