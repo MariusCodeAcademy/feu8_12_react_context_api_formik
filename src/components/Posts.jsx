9f62911cfba5a8d4c819207b4d5ef5a26fce5023
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-22 12:01:25 +0300

    Posts

---
+++ src/components/Posts.jsx
@@ -0,0 +1,14 @@
+export default function Posts() {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        exercitationem aperiam et eum harum sapiente aspernatur pariatur ea ab,
+        sunt fugit soluta voluptatem impedit voluptate maiores aliquid eligendi
+        laudantium adipisci?
+      </p>
+      <button>Logout</button>
+    </div>
+  );
+}
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Wrap from '../styled/Wrap.styled';
+import Posts from '../components/Posts';
 
 const Title = styled.h1`
   font-size: 35px;
@@ -10,6 +11,7 @@ export default function AboutPage() {
     <Wrap>
       <Title>AboutPage</Title>
       <p>Welcome to our page</p>
+      <Posts />
     </Wrap>
   );
 }
