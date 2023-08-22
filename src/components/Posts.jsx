2ce7c6dc9257148b376632eafb1fbf60287d00a7
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-22 12:02:35 +0300

    uzd

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -8,6 +8,8 @@ export default function Posts() {
         sunt fugit soluta voluptatem impedit voluptate maiores aliquid eligendi
         laudantium adipisci?
       </p>
+      {/* 1. rodyti tik jei esam prisijunge */}
+      {/* 2. paspaudus atloginti useri nuo sistemos */}
       <button>Logout</button>
     </div>
   );
