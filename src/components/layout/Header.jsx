11952e3b8720d94534d5afb0df8121540687d6ee
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-22 08:58:24 +0300

    active

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,6 +5,8 @@ import Wrap from '../../styled/Wrap.styled';
 const HeaderContainer = styled(Wrap)`
   display: flex;
   gap: 15px;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Logo = styled(Link)`
@@ -12,15 +14,30 @@ const Logo = styled(Link)`
   padding: 0.2em 0;
 `;
 
+const Nav = styled.nav``;
+
+const OneLink = styled(NavLink)`
+  font-size: 17px;
+  padding: 0.3em 0.8em;
+  border: 1px solid transparent;
+
+  &:hover {
+    background-color: #b3b3b3;
+  }
+
+  &.active {
+    border: 1px solid #b3b3b3;
+  }
+`;
+
 export default function Header() {
   return (
     <HeaderContainer as={'header'}>
       <Logo to={'/'}>OurLogo</Logo>
-      <nav>
-        <a href='' className='navLink'>
-          link
-        </a>
-      </nav>
+      <Nav>
+        <OneLink to={'/'}>Home</OneLink>
+        <OneLink to={'/about'}>About us</OneLink>
+      </Nav>
     </HeaderContainer>
   );
 }
