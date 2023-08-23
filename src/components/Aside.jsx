b2e15a1091a5f8552cacc77fc61a469dc8434379
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 08:32:03 +0300

    show user if lged in

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,14 @@
+import { useContext } from 'react';
 import styled from 'styled-components';
+import AuthContext from '../store/AuthContext';
 
 export default function Aside() {
-  return <div>Aside</div>;
+  const ctx = useContext(AuthContext);
+  console.log('ctx ===', ctx);
+  return (
+    <div>
+      <h3>Additional info:</h3>
+      {ctx.isUserLoggedIn && <p>User: {ctx.email}</p>}
+    </div>
+  );
 }
