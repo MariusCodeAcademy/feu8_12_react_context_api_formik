09b501b22f825b4dcc5e71f8f20f6b367f78ace2
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 08:23:10 +0300

    ignore

--- src/store/AuthContext.jsx
+++ src/store/AuthContext.jsx
@@ -3,7 +3,8 @@ import { createContext } from 'react';
 const AuthContext = createContext({
   email: '',
   isUserLoggedIn: false,
-  login(email) {},
+  // eslint-disable-next-line no-unused-vars
+  login(_email) {},
   logout() {},
 });
 AuthContext.displayName = 'MusuAutentifikacija';
