238e7365ed68ba4409c7e0399f0d525e6dd275de
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-23 08:28:05 +0300

    Aside placement

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,28 @@
 import styled from 'styled-components';
 import Wrap from '../styled/Wrap.styled';
+import Aside from '../components/Aside';
 
 const Title = styled.h1`
   font-size: 35px;
   font-weight: 500;
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: auto 30%;
+  gap: 15px;
+`;
+
 export default function HomePage() {
   return (
     <Wrap>
-      <Title>HomePage</Title>
-      <p>Welcome to our page</p>
+      <Grid>
+        <div>
+          <Title>HomePage</Title>
+          <p>Welcome to our page</p>
+        </div>
+        <Aside />
+      </Grid>
     </Wrap>
   );
 }
